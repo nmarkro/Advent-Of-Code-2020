b881b5bc095659d6c031aac1335c6ed128e0e2aa
nmarkro <nmarkro@gmail.com>
2020-12-01 10:07:29 -0600

    upload day 1 solution

---
+++ day-1/report_repair.py
@@ -0,0 +1,27 @@
+def parse_report():
+    report = []
+    with open("./input.txt", "r") as f:
+        for line in f.readlines():
+            report.append(int(line))
+    return report
+
+# Part 1
+def find_product_of_two_by_sum(report, sum_to_find):
+    for num in report:
+        search_value = sum_to_find - num
+        if report.count(search_value) > 0:
+            return num * search_value
+    return None
+
+# Part 2
+def find_product_of_three_by_sum(report, sum_to_find):
+    for i, num in enumerate(report):
+        for j in range(i + 1, len(report)):
+            search_value = sum_to_find - num - report[j]
+            if report.count(search_value) > 0:
+                return num * search_value * report[j]
+    return None
+
+expense_report = parse_report()
+print("Part 1 Answer: {}".format(find_product_of_two_by_sum(expense_report, 2020)))
+print("Part 2 Answer: {}".format(find_product_of_three_by_sum(expense_report, 2020)))
